4a0eeb4c8ce174d119d549702448556e9bface31
Chima <xhimat123@gmail.com>
2016-05-01 22:34:57 +0100

    added open shift production info

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var Hapi = require('hapi');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+    host: server_ip_address, 
+    port: server_port 
 });
 
 server.register([require('vision'), require('inert'),require('lout')], function (err) {
