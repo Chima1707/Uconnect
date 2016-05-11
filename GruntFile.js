8d19bd945f6fa0193218672ff42cc790501dacc9
Chima <xhimat123@gmail.com>
2016-05-11 15:39:30 +0100

    commit after formatting

--- GruntFile.js
+++ GruntFile.js
@@ -1,33 +1,32 @@
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   // Project configuration.
-  
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-   nodemon: {
-  dev: {
-    script: 'index.js',
-    options: {
-      args: ['dev'],
-      nodeArgs: ['--debug'],
-      callback: function (nodemon) {
-        nodemon.on('log', function (event) {
-          console.log(event.colour);
-        });
-      },
-      env: {
-        PORT: '8181'
-      },
-      cwd: __dirname,
-      ignore: ['node_modules/**'],
-      ext: 'js,coffee',
-      watch: ['.'],
-      delay: 1000,
-      legacyWatch: true
+    nodemon: {
+      dev: {
+        script: 'index.js',
+        options: {
+          args: ['dev'],
+          nodeArgs: ['--debug'],
+          callback: function (nodemon) {
+            nodemon.on('log', function (event) {
+              console.log(event.colour)
+            })
+          },
+          env: {
+            PORT: '8181'
+          },
+          cwd: __dirname,
+          ignore: ['node_modules/**'],
+          ext: 'js,coffee',
+          watch: ['.'],
+          delay: 1000,
+          legacyWatch: true
+        }
+      }
     }
-  }
+  })
+  grunt.loadNpmTasks('grunt-nodemon')
+  grunt.registerTask('default', ['nodemon:dev'])
 }
-});
-grunt.loadNpmTasks('grunt-nodemon');
-grunt.registerTask('default', ['nodemon:dev'])
-};
--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 #!/bin/env node
-'use strict';
+'use strict'
 
-var Hapi = require('hapi');
+var Hapi = require('hapi')
 
 // Create a server with a host and port
-var server = new Hapi.Server();
+var server = new Hapi.Server()
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-server.connection({ 
-    host: server_ip_address, 
-    port: server_port 
-});
+server.connection({
+  host: server_ip_address,
+  port: server_port
+})
 
+server.route(require('./lib/routes.js'))
 
-   server.route(require('./lib/routes.js'))
+server.start(function (err) {
+  if (err) {
+    throw err
+  }
 
-   server.start(function (err) {
-       
-    if (err) {
-        throw err;
-    }
-    
-    console.log('Server running at:', server.info.uri);
-    
-  });
+  console.log('Server running at:', server.info.uri)
+})
